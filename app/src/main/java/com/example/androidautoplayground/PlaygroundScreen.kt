a8e6a1f28db3dcdbc24f1d47eb8fa143bd849703
Martin Styk <mstyk@sygic.com>
2021-10-07 11:50:28 +0200

    Sample simplified

--- app/src/main/java/com/example/androidautoplayground/PlaygroundScreen.kt
+++ app/src/main/java/com/example/androidautoplayground/PlaygroundScreen.kt
@@ -2,24 +2,22 @@ package com.example.androidautoplayground
 
 import androidx.car.app.CarContext
 import androidx.car.app.Screen
-import androidx.car.app.model.Action
-import androidx.car.app.model.ActionStrip
-import androidx.car.app.model.CarIcon
-import androidx.car.app.model.Template
+import androidx.car.app.model.*
 import androidx.car.app.navigation.model.NavigationTemplate
 
 class PlaygroundScreen(carContext: CarContext) : Screen(carContext) {
 
     override fun onGetTemplate(): Template {
-        val actionStrip = ActionStrip.Builder()
+
+        return MessageTemplate.Builder("Test action")
             .addAction(
                 Action.Builder()
-                    .setIcon(CarIcon.ALERT)
-                    .setOnClickListener { carContext.finishCarApp() }
-                    .build())
+                    .setOnClickListener { }
+                    .setTitle("Action")
+                    .build()
+                )
+            .setTitle("Playground")
             .build()
-
-        return NavigationTemplate.Builder().setActionStrip(actionStrip).build()
     }
 
 }
